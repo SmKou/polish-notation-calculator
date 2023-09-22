3dbc36dfb077bb1bcb54934670edd5e6df7a5cf0
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-21 21:17:26 -0700

    Add App (root)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-
+import { useState } from 'react'
+import Calculator from "./components/Calculator/Calculator"
+import MemoryCache from "./components/MemoryCache/MemoryCache"
+import OperationsTree from "./components/OperationsTree/OperationsTree"
 
 function App() {
+    const [total, setTotal] = useState(0)
+    const [calculation, setCalculation] = useState('')
 
     return <>
+        <header>
+            <h1>Polish Notation Calculator</h1>
+        </header>
+        <main>
+            <section>
+                <MemoryCache total={total} calculation={calculation} setTotal={setTotal} setCalculation={setCalculation} />
+                <Calculator total={total} calculation={calculation} setTotal={setTotal} setCalculation={setCalculation} />
+            </section>
+            <section><OperationsTree total={total} calculation={calculation} /></section>
+        </main>
     </>
 }
 
